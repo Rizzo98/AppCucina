a992125ad4ddf7ef6e0ffaa739d3872613e8f460
Rizzo98 <andrearizzardi98@gmail.com>
2020-05-12 16:21:05 +0200

    Implementata tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController,ModalController } from '@ionic/angular';
+import {DatabaseService} from '../database.service'
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,59 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  recipes:any[]
+  loading:boolean
+  filtered:any[]
+  
+  constructor(public navCtrl: NavController, private route: ActivatedRoute, private db : DatabaseService) {
+    this.recipes=[]
+    this.loading=false
+  }
+
+  ngOnInit() {
+    if(this.recipes.length==0){
+      this.loading=true
+      this.db.getRecipes((c)=>{
+        this.recipes=c
+        this.filtered=c
+        this.loading=false
+        console.log(this.recipes)
+      },undefined,10000,undefined,undefined,undefined)
+    }
+  }
+
+  recipeClick(r){
+    let p = new Promise((resolve,reject)=>{
+        let file = new Array(r.files.length).fill(0)
+        r.files.forEach(f => {
+          let index = parseInt(f.split('%2F')[1].split('.')[0])-1
+          file[index]=f
+        });
+        resolve(file)  
+    })
+    .then(file=>{
+      r.files=file
+      this.navCtrl.navigateForward('/recipe-detail',{ queryParams: { recipe :  JSON.stringify(r) } })
+    })
+  }
+
+  filterList(e){
+    this.filtered = this.recipes
+    const searchTerm = e.srcElement.value;
+
+    if (!searchTerm) {
+      return;
+    }
+
+    this.filtered = this.filtered.filter(currentGoal => {
+      if (currentGoal.titolo && searchTerm) {
+        if (currentGoal.titolo.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
+  }
+
 
 }
